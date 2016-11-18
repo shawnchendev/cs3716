ff30a3248861dc59a65103d790ffd5704f9806d5
Diego <dazc35@mun.ca>
2016-11-18 16:34:33 -0330

    Summary
    The match class is the same one that Andrew made with a few changes
    The treeS class is just for the structure of the tree for single
    elimination
    Brackets is the new way of creating the single elimination as a binary
    tree by using an instance of treeS
    The team class is a simple implementation just for testing purposes

---
+++ cs3716/src/cs3716/team.java
@@ -0,0 +1,14 @@
+package cs3716;
+//Simple constructor just for testing purposes
+public class team{
+	private String name;
+	public team (String n){
+		this.name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
