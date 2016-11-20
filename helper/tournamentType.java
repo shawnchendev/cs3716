2ff63f25a8e34d09b3cd9343acb9608c2e275472
Andrew <arw405@mun.ca>
2016-11-20 14:13:55 -0330

    Reorganized the repo into two packages; one for helper classes and one for matchmaking classes.

---
+++ helper/tournamentType.java
@@ -0,0 +1,5 @@
+package volleyballTournament;
+
+public interface tournamentType {
+
+}
---
+++ matchmaking/Match.java
@@ -0,0 +1,34 @@
+
+public class Match {
+	int team1,team2,score1,score2;
+	public Match(int t1,int t2){
+		team1=t1;
+		team2=t2;
+		score1=1;
+		score2=2;//DEFAULT SCORES
+	}
+	public int getTeam1(){
+		return team1;
+	}
+	public int getTeam2(){
+		return team2;
+	}
+	public void setTeam1(int t1){
+		team1 = t1;
+	}
+	public void setTeam2(int t2){
+		team2 = t2;
+	}
+	public void setScore1(int s1){
+		score1=s1;
+	}
+	public void setScore2(int s2){
+		score2=s2;
+	}
+	public int getScore1(){
+		return score1;
+	}
+	public int getScore2(){
+		return score2;
+	}
+}
